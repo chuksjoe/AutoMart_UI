0e186659d2f8843c6b29c11e743d999855eee8bc
Chukwunonso Orjiakor <chuksjoe@live.com>
2019-07-02 01:24:17 +0100

    feature(add-mrketplace-pg):add mrketplace page with some modals
    - add the marketplace page and consume the api endpoint for getting list of car Ads
    - activate the filter sidebar such that it consumes the endpoint for getting a queried list of car Ads
    - enable the modal for previewing a specific car Ad
    - enable the modal for placing a purchase order by consuming the relevant endpoint from challenge 3
    
    [Finishes #167013295, #167023403, #167023373]

--- UI/js/marketplace.js
+++ UI/js/marketplace.js
@@ -99,35 +99,53 @@ const openFraudModal = (params) => {
 
 // used to get details of a car from the database
 const getCarDetils = (carId) => {
+  const carPreviewModal = document.querySelector('#car-preview-overlay .modal-body');
+  const carDetails = document.querySelector('#car-preview-overlay .car-view-detail');
+  const mainDesc = document.querySelector('#car-preview-overlay .car-view-main-desc');
+  const otherInfo = document.querySelector('#car-preview-overlay .other-info');
+
+  carPreviewModal.innerHTML = '<div id="loading"><img src="../images/loader.gif" /></div>';
   fetch(`${urlPrefix}/api/v1/car/${carId}`)
   .then(res => res.json())
   .then((response) => {
     if (response.status === 200) {
       const car = response.data;
       const {
-        id, name, img_url, manufacturer, model, year, state,
-        owner_id, owner_name, price, body_type, fuel_type, mileage,
-        color, transmission_type, fuel_cap, created_on, doors,
+        id, name, img_url, manufacturer, model, year, state, owner_id, owner_name, price,
+        body_type, fuel_type, mileage, color, transmission_type, fuel_cap, created_on, doors,
+        description, ac, arm_rest, air_bag, dvd_player, fm_radio, tinted_windows,
       } = car;
-      const desc = document.querySelector('#car-preview-overlay .car-view-main-desc');
       document.querySelector('#car-preview-overlay .modal-header').innerHTML = name;
-      document.querySelector('#car-preview-overlay .added-date').innerHTML = `Added on: ${created_on}`;
-      document.querySelector('#car-preview-overlay img').setAttribute('src', img_url);
-      desc.innerHTML = `<p class="c-price">Price: &#8358 ${parseInt(price, 10).toLocaleString('en-US')}</p>
-              <div class="prop-list flex-container">
-                <p class="prop"><b>Make:</b><br>${manufacturer}</p>
-                <p class="prop"><b>Model:</b><br>${model}</p>
-                <p class="prop"><b>State:</b><br>${state}</p>
-                <p class="prop"><b>Body Type:</b><br>${body_type}</p>
-                <p class="prop"><b>Color:</b><br>${color}</p>
-                <p class="prop"><b>Year:</b><br>${year}</p>
-                <p class="prop"><b>Fuel Type:</b><br>${fuel_type}</p>
-                <p class="prop"><b>Fuel Capacity:</b><br>${fuel_cap}L</p>
-                <p class="prop"><b>Mileage:</b><br>${mileage.toLocaleString('en-US')}km</p>
-                <p class="prop"><b>Transmission:</b><br>${transmission_type}</p>
-                <p class="prop"><b>Posted By:</b><br>${(owner_id === parseInt(user_id, 10) ? 'Me' : owner_name)}</p>
-                <p class="prop"><b>Doors:</b><br>${doors} doors</p>
-              </div>`;
+      document.querySelector('#car-preview-overlay .added-date').innerHTML = `Added on: ${configDate(created_on)}`;
+
+      carDetails.innerHTML = `<div class="car-view-image"><img src="${img_url}" alt="${name}"></div>`;
+      mainDesc.innerHTML = `
+      <p class="c-price">Price: &#8358 ${parseInt(price, 10).toLocaleString('en-US')}</p>
+      <div class="prop-list flex-container">
+        <p class="prop"><b>Make:</b><br>${manufacturer}</p>
+        <p class="prop"><b>Model:</b><br>${model}</p>
+        <p class="prop"><b>State:</b><br>${state}</p>
+        <p class="prop"><b>Body Type:</b><br>${body_type}</p>
+        <p class="prop"><b>Color:</b><br>${color}</p>
+        <p class="prop"><b>Year:</b><br>${year}</p>
+        <p class="prop"><b>Fuel Type:</b><br>${fuel_type}</p>
+        <p class="prop"><b>Fuel Capacity:</b><br>${fuel_cap}L</p>
+        <p class="prop"><b>Mileage:</b><br>${mileage.toLocaleString('en-US')}km</p>
+        <p class="prop"><b>Transmission:</b><br>${transmission_type}</p>
+        <p class="prop"><b>Posted By:</b><br>${(owner_id === parseInt(user_id, 10) ? 'Me' : owner_name)}</p>
+        <p class="prop"><b>Doors:</b><br>${doors} doors</p>
+      </div>`;
+      otherInfo.innerHTML = `
+      <div class="description">
+        <h3>Description</h3><hr>
+        <p>${description}</p>
+      </div>
+      <div class="features">
+        <h3>Features</h3><hr>
+        <p>${ac ? '<label>Air Condition</label>' : ''}${arm_rest ? '<label>Arm Rest</label>' : ''}
+        ${air_bag ? '<label>Air Bag</label>' : ''}${dvd_player ? '<label>DVD Player</label>' : ''}
+        ${fm_radio ? '<label>FM Radio</label>' : ''}${tinted_windows ? '<label>Tinted Glasses</label>' : ''}</p>
+      </div>`;
       const btnGrp = document.createElement('div');
       const orderBtn = document.createElement('button');
       const flagBtn = document.createElement('button');
@@ -153,7 +171,11 @@ const getCarDetils = (carId) => {
       btnGrp.appendChild(orderBtn);
       btnGrp.appendChild(flagBtn);
 
-      desc.appendChild(btnGrp);
+      mainDesc.appendChild(btnGrp);
+      carPreviewModal.innerHTML = null;
+      carDetails.appendChild(mainDesc);
+      carDetails.appendChild(otherInfo);
+      carPreviewModal.appendChild(carDetails);
     } else {
       const message = document.querySelector('#notification-overlay .message');
       message.innerHTML = response.error;
@@ -165,12 +187,13 @@ const getCarDetils = (carId) => {
 
 const fetchCarAds = (url, msgIfEmpty) => {
   const carList = document.querySelector('.car-list');
-  carList.innerHTML = null;
+  carList.innerHTML = '<div id="loading"><img src="../images/loader.gif" /></div>';
   fetch(url)
   .then(res => res.json())
   .then((response) => {
     const res = response;
     if (res.data.length > 0) {
+      carList.innerHTML = null;
       res.data.map((car) => {
         const {
           id, name, model, body_type, manufacturer, year, state, price, img_url, owner_id,
